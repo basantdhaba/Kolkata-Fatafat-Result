4a3d9813d619d7cd12378248b9f744632fa40c04
basantdhaba <basantdhaba@gmail.com>
2025-03-17 01:14:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-import dotenv from '.env';
-
-dotenv.config();
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
-  },
-}));
+    envCompatible()
+  ]
+});
