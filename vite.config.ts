76fe755f74cd36155d4dfa5cf3e3f76187aba96e
basantdhaba <basantdhaba@gmail.com>
2025-03-17 09:13:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  base: './', // Ensure proper relative paths for deployment
-  plugins: [
-    react(),
-    envCompatible()
-  ]
+  base: '/', // Fixes incorrect asset paths
+  plugins: [react(), envCompatible()]
 });
