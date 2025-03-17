3f85146c68ae0757ff84f3e8ed4dc80648c1e2c1
basantdhaba <basantdhaba@gmail.com>
2025-03-17 09:02:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import envCompatible from 'vite-plugin-env-compatible';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensure proper relative paths for deployment
   plugins: [
     react(),
     envCompatible()
